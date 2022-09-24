07e760e102a046d2f627c8852ba795f626f5ae0a
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2022-09-24 22:47:06 +0200

    initial

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <cstring>
+#include <string>
+#include <filesystem>
+
+#ifdef _WIN32
+bool windows = true;
+#include <libloaderapi.h>
+#endif
+
+#ifdef __unix__
+bool windows = false;
+#endif
+
+int main(int argc, char *argv[])
+{
+	if (windows)
+	{
+		char pBuf[256];
+
+		GetModuleFileNameA(NULL, pBuf, sizeof(pBuf));
+
+		printf(pBuf);
+	}
+	else
+	{
+		std::filesystem::canonical("/proc/self/exe/");
+	}
+
+	if (argc <= 1)
+	{
+		printf("Welcome to PNVC, insert help here blah blah...");
+	}
+	else
+	{
+		// Add command
+		if (strcmp(argv[1], "add") == 0)
+		{
+			if (argc >= 2)
+			{
+			}
+		}
+
+		// Commit command
+		if (strcmp(argv[1], "commit") == 0)
+		{
+			if (argc >= 2)
+			{
+			}
+		}
+	}
+}
